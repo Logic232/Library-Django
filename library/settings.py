3cd2095de19535563dc553c8304deb0dbb7e8d81
Adrian <alange1337@gmail.com>
2022-05-22 16:38:31 +0200

    poprawki-password

--- library/settings.py
+++ library/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'base_db',
         'USER': 'postgres',
-        'PASSWORD': 'Papa1234$',
+        'PASSWORD': 'kowalkox',
         'HOST': 'localhost',
         'PORT': '5432',
     }
