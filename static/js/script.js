eab99891a7dd6586cef8aebb9aa4d62c135dfe4f
Mateusz Kowalczyk <kowalczyk_mateusz@poczta.fm>
2022-05-25 23:41:50 +0200

    adding searching functionality and some styling

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,9 @@ const e = (() => {
     let qsa = (c) => [...document.querySelectorAll(c)];
     return {
         profileImage: qs(".profile-picture"),
-        navigationWrapper: qs(".navigation")
+        navigationWrapper: qs(".navigation"),
+        findBookInput: qs(".find-book"),
+        booksToFind: qsa(".book-to-find")
     };
 })();
 
@@ -11,4 +13,26 @@ const ToggleMenu = () => {
     e.navigationWrapper.classList.toggle("active");
 };
 
+const FindBookHandler = ({ target }) => {
+    let value = target.value.toLowerCase();
+    e.booksToFind.forEach((el) => {
+        let parsedHTML = el.innerHTML.toLowerCase();
+        parsedHTML.includes(value) ? el.classList.remove("hidden") : el.classList.add("hidden");
+    });
+};
+if (window.location.pathname === "/loans/")
+    e.findBookInput.addEventListener(
+        "input",
+        debounce((event) => FindBookHandler(event))
+    );
 e.profileImage.addEventListener("click", ToggleMenu);
+
+function debounce(func, timeout = 500) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, timeout);
+    };
+}
