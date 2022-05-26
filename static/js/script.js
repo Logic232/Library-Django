75bc8d6f73652692482e4dc0e6881d8b3e4b5eb9
Mateusz Kowalczyk <kowalczyk_mateusz@poczta.fm>
2022-05-26 22:15:48 +0200

    refractoring js file

--- static/js/script.js
+++ static/js/script.js
@@ -11,53 +11,56 @@ const e = (() => {
     };
 })();
 
-console.log(e.bookYearInput);
+window.addEventListener("load", () => {
+    if (e.bookYearInput) ChangeMaxNumberValue();
+    if (e.booksList.length === 0) DetectIfImagesAreValid();
+    if (e.findBookInput)
+        e.findBookInput.addEventListener(
+            "input",
+            debounce((event) => FindBookHandler(event))
+        );
+    e.profileImage.addEventListener("click", ToggleMenu);
 
-const ChangeMaxNumberValue = (() => {
-    if (!e.bookYearInput) return;
-
-    e.bookYearInput.setAttribute("max", "2022");
-})();
-const CheckImage = (image) => {
-    const request = new XMLHttpRequest();
-    request.open("GET", image.src, true);
-    request.send();
-    request.onload = () => {
-        console.log(request.status);
-        if (request.status !== 200) {
-            image.src = "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
-            image.classList.add("border");
-        }
+    const ChangeMaxNumberValue = () => {
+        e.bookYearInput.setAttribute("max", "2022");
     };
-};
-const DetectIfImagesAreValid = (() => {
-    if (e.booksList.length === 0) return;
-    e.booksList.map((book) => CheckImage(book));
-})();
-const ToggleMenu = () => {
-    e.navigationWrapper.classList.toggle("active");
-};
 
-const FindBookHandler = ({ target }) => {
-    let value = target.value.toLowerCase();
-    e.booksToFind.forEach((el) => {
-        let parsedHTML = el.innerHTML.toLowerCase();
-        parsedHTML.includes(value) ? el.classList.remove("hidden") : el.classList.add("hidden");
-    });
-};
-if (e.findBookInput)
-    e.findBookInput.addEventListener(
-        "input",
-        debounce((event) => FindBookHandler(event))
-    );
-e.profileImage.addEventListener("click", ToggleMenu);
-
-function debounce(func, timeout = 300) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, args);
-        }, timeout);
+    const CheckImage = (image) => {
+        const request = new XMLHttpRequest();
+        request.open("GET", image.src, true);
+        request.send();
+        request.onload = () => {
+            console.log(request.status);
+            if (request.status !== 200) {
+                image.src = "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+                image.classList.add("border");
+            }
+        };
     };
-}
+
+    const DetectIfImagesAreValid = () => {
+        e.booksList.map((book) => CheckImage(book));
+    };
+
+    const ToggleMenu = () => {
+        e.navigationWrapper.classList.toggle("active");
+    };
+
+    const FindBookHandler = ({ target }) => {
+        let value = target.value.toLowerCase();
+        e.booksToFind.forEach((el) => {
+            let parsedHTML = el.innerHTML.toLowerCase();
+            parsedHTML.includes(value) ? el.classList.remove("hidden") : el.classList.add("hidden");
+        });
+    };
+
+    function debounce(func, timeout = 300) {
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                func.apply(this, args);
+            }, timeout);
+        };
+    }
+});
